b1600acbf8a81267afad25d6f2fd90364e6077bb
David Tomčík <david.tomcik@external.t-mobile.cz>
2021-07-30 12:41:28 +0200

    3.  tasks

--- src/main/java/com/recruitment/sandbox/SandboxApplication.java
+++ src/main/java/com/recruitment/sandbox/SandboxApplication.java
@@ -3,9 +3,7 @@ package com.recruitment.sandbox;
 import com.recruitment.sandbox.model.Customer;
 import com.recruitment.sandbox.model.Device;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,7 +16,11 @@ public class SandboxApplication {
 		List<Customer> customers = new ArrayList<>();
 		customers.addAll(createCustomers());
 
-		// zde napište implementaci, která v kolekci customers nalezne libovolného prvního zákazníka, který vlastní fixní linku a vypíše jeho jméno do konzole.
+		// 1. V kolekci customers najděte libovolného prvního zákazníka, který vlastní fixní linku a vypíšte jeho jméno do konzole.
+
+		// 2. Do konzole vypište seznam jmen zařízení, které jsou unikátní (žádné jméno se ve výsledném seznamu nesmí opakovat) a zároveň dražší než 20000Kč.
+
+		// 3. Výsledný seznam nalezených jmen zařízení seřaďte abecedně a vypište do konzole
 
 	}
 
@@ -26,7 +28,8 @@ public class SandboxApplication {
 	/**
 	 * creates two instances of Customer:
 	 *  1 - Pavel , has two devices (1. iPhone, 2. fixedLine)
-	 *  2 - Karel, has one device (1. samsungGalaxy)
+	 *  2 - Karel, has one device (1. Alcatel)
+	 *  3 - Pepa , has one device (1. iPhone)
 	 * @return - collection of customers
 	 */
 	private static Collection<Customer> createCustomers() {
@@ -35,20 +38,29 @@ public class SandboxApplication {
 		devicesPavel.add(Device.builder().id("device_1").name("iPhone").mobile(true).price(new BigDecimal("29000")).build());
 		devicesPavel.add(Device.builder().id("device_2").name("fixedLine").mobile(false).price(new BigDecimal("5000")).build());
 		customers.add(
-			Customer.builder()
-				.id("customer_1")
-				.name("Pavel")
-				.devices(devicesPavel)
-				.build());
+				Customer.builder()
+						.id("customer_1")
+						.name("Pavel")
+						.devices(devicesPavel)
+						.build());
 
 		List<Device> devicesKarel = new ArrayList<>();
-		devicesKarel.add(Device.builder().id("device_3").name("samsungGalaxy").mobile(true).price(new BigDecimal("21000")).build());
+		devicesKarel.add(Device.builder().id("device_3").name("Alcatel").mobile(true).price(new BigDecimal("21000")).build());
 		customers.add(
-			Customer.builder()
-				.id("customer_2")
-				.name("Karel")
-				.devices(devicesKarel)
-				.build());
+				Customer.builder()
+						.id("customer_2")
+						.name("Karel")
+						.devices(devicesKarel)
+						.build());
+
+		List<Device> devicesPepa = new ArrayList<>();
+		devicesPepa.add(Device.builder().id("device_1").name("iPhone").mobile(true).price(new BigDecimal("29000")).build());
+		customers.add(
+				Customer.builder()
+						.id("customer_3")
+						.name("Pepa")
+						.devices(devicesPepa)
+						.build());
 
 		return customers;
 	}
